38b897a3d2e050bd8e5a9bc8cfdd08fe8cfe405b
Yehdhih ANNA <yehdihouldanna@gmail.com>
2025-07-11 17:26:00 +0000

    improving train file to use aws and adding aws params

--- src/train.py
+++ src/train.py
@@ -6,21 +6,12 @@ from sklearn.metrics import accuracy_score,confusion_matrix,classification_repor
 import os
 from urllib.parse import urlparse
 from sklearn.model_selection import train_test_split,GridSearchCV
-
-
-# dagshub experiment tracking uri : found in experiment onglet
-os.environ["MLFLOW_TRACKING_URI"] = "https://dagshub.com/yehdihouldanna/mlpipeline-mlflow-dvc.mlflow"
-
-os.environ["MLFLOW_TRACKING_USERNAME"] = 'yehdihouldanna' #dagsub username
-
-# this is the token in your profile (default) and you can also copy it from the dvc onglet in the repository
-os.environ["MLFLOW_TRACKING_PASSWORD"]= "28fdecc4cae73c08dadfd562c7bfe688f20b9a78"
-
-
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 import mlflow
 from mlflow.models import infer_signature
-import mlflow.pyfunc
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import boto3
 
 def hyperparameter_tuning(X_train,y_train,param_grid):
     rf = RandomForestClassifier()
@@ -28,28 +19,31 @@ def hyperparameter_tuning(X_train,y_train,param_grid):
     grid_search.fit(X_train,y_train)
     return grid_search
 
-train_params = yaml.safe_load(open("params.yaml"))['train']
-dagshub_params = yaml.safe_load(open("params.yaml"))['dagshub']
-
 def train(data_path,model_path,random_state,n_estimators,max_depth):
     data = pd.read_csv(data_path)
     X = data.drop(columns=["Outcome"])
     y = data["Outcome"]
 
-    mlflow.set_tracking_uri(dagshub_params["MLFLOW_TRACKING_URI"])
+    mlflow.set_tracking_uri(mlflow_params["MLFLOW_TRACKING_URI"])
     with mlflow.start_run():
+
         X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
 
         signature = infer_signature(X_train,y_train)
 
         # Define hyper params grid: 
-        
         param_grid = {
             # 'n_estimators' : [100, 200],
             # 'max_depth' : [5,10,None],
             # 'min_samples_split' : [2,5],
             'min_samples_leaf' : [1,2]
         }
+
+        # Logging experiment details : 
+        mlflow.set_tag("mlflow.runName","Best RandomForestClassifier")
+        mlflow.set_tag("experiment_type","grid_search_cv for best hyperparameters")
+        mlflow.set_tag("model_type","RandomForestClassifier")
+        mlflow.set_tag("description","RandomForestClassifier with GridSearchCV For Hyperparameter Tuning")
 
         # Perform hte hyperparams tuning
         grid_search = hyperparameter_tuning(X_train,y_train,param_grid)
@@ -69,53 +63,42 @@ def train(data_path,model_path,random_state,n_estimators,max_depth):
 
         # log the confusions matrix and  classification report : 
         cm = confusion_matrix(y_test,y_pred)
-        cr = classification_report(y_test,y_pred)
+        classification_rep = classification_report(y_test,y_pred,output_dict=True)
 
         mlflow.log_text(str(cm),"confusion_matrix.txt")
-        mlflow.log_text(str(cr),"classification_report.txt")
+        mlflow.log_text(str(classification_rep),"classification_report.txt")
 
-        # # To solve some errors related to wrong file type while logging the model
-        # tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-        # if tracking_url_type_store !="file":
-        #     mlflow.sklearn.log_model(best_model,"model",registered_model_name="Best_model")
-        # else : 
-        # mlflow.sklearn.log_model(
-        #     sk_model=best_model,
-        #     artifact_path="model",
-        #     signature=signature
-        # )
         
-        # Replace the mlflow.sklearn.log_model() call with:
-        
-        #? mlflow.sklearn.save_model(
-            #? sk_model=best_model,
-            #? path="model_path",
-        #? )
-        #? # Then log the model as an artifact
-        #? mlflow.log_artifacts("model_path")
+        for label, metrics in classification_rep.items():
+            if isinstance(metrics,dict): # for precision recall, f1-score, etc,
+                for metric, value in metrics.items():
+                    mlflow.log_metric(f"{label}_{metric}",value)
 
-        import joblib
-        os.makedirs(os.path.dirname(model_path), exist_ok=True)
-        joblib.dump(best_model, model_path)
-        print(f"Model saved to {model_path}")
+        conf_matrix = confusion_matrix(y_test,y_pred)
+        plt.figure(figsize=(8,6))
+        sns.heatmap(conf_matrix,annot=True, fmt="d", cmap="Blues")
+        plt.xlabel("Predicted")
+        plt.ylabel("Actual")
+        plt.title("Confusion_matrix")
 
-        mlflow.log_artifact(model_path)
+        # artifacts
+        plt.savefig("confusion_matrix.png")
+        mlflow.log_artifact("confusion_matrix.png")
 
-
-        # creaete the directory to save the model : 
-        # os.makedirs(os.path.dirname(model_path),exist_ok=True)
-
-        # filename=model_path
-        # pickle.dump(best_model,open(filename,"wb"))
-        # print(f"Model saved to {model_path}")
+        # logging the model :
+        mlflow.sklearn.log_model(best_model, "Best_RandomForestClassifier",
+                                 registered_model_name="Best_RandomForestClassifier",
+                                 signature=signature)
 
 
 if __name__=="__main__":
+
+    train_params = yaml.safe_load(open("params.yaml"))['train']
+    mlflow_params = yaml.safe_load(open("params.yaml"))['mlflow']
+    aws_params = yaml.safe_load(open("params.yaml"))['aws']
+    # Set AWS credentials as environment variables for MLflow and boto3 to use
+    os.environ['AWS_ACCESS_KEY_ID'] = aws_params['aws_access_key_id']
+    os.environ['AWS_SECRET_ACCESS_KEY'] = aws_params['aws_secret_access_key']
+    os.environ['AWS_DEFAULT_REGION'] = aws_params['region_name']
+
     train(train_params["data"],train_params["model_path"],train_params["random_state"], train_params["n_estimators"],train_params['max_depth'])
-
-
-
-
-
-
-
